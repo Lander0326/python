6bf8e797c313d68914fae395ab7977fb2b1347b7
Lander0326 <53064041+Lander0326@users.noreply.github.com>
2022-10-11 01:47:44 +0800

    Delete 上課寫的程式 directory

--- 上課寫的程式/2022-08-24-SimpleGUI-01.py
+++
@@ -1,9 +0,0 @@
-import PySimpleGUI as sg
-
-layout = [
-    [sg.Text('Text'),sg.Spin(['item 1','item 2'])],
-    [sg.Button('button')],
-    [sg.Input()]
-]
-
-sg.Window('Converter',layout).read()
--- 上課寫的程式/oneline.py
+++
@@ -1,3 +0,0 @@
-import PySimpleGUI as sg
-
-print(sg.Window('', [[sg.Input(),sg.Button('Ok'),sg.Button('Cancel')]]).read())
